21fff3073096b2e220c3ab559e1e4b848979b017
CGM123sherry <g.christinemurithi@gmail.com>
2024-07-03 04:52:32 +0300

    completed the scoe lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+customerName = customerName.toUpperCase ();
+}
+function setBestCustomer(){
+bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+const leastFavouriteCustomer = 'lulu';
+// function changeleastFavouriteCustomer(){
+//     const leastFavouriteCustomer = 'hassan';
+//     throw new TypeError('Assignment to constant variable.');
+
+// }
+function changeLeastFavoriteCustomer(){
+    const leastFavouriteCustomer = 'abel';
+    throw new TypeError('Assignment to constant variable.');
+
+
+}
